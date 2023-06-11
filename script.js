dde22b408c4589d1f0f162b515c2a356f7edd0fa
alife-echo <alifesilva90@gmail.com>
2023-06-11 11:18:57 -0300

    login,sign-in,register

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',()=>{
+     let loader = document.querySelector('.spinner')
+     loader.style.display = 'none'
+     let content = document.querySelector('.form-container')
+     content.style.display='flex'
+})
