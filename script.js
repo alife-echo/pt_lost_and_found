77293df317dc5555ef6927be1297786746250ad4
alife-echo <alifesilva90@gmail.com>
2023-06-19 11:07:18 -0300

    proto lost and found

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load',()=>{
-     let loader = document.querySelector('.spinner')
-     loader.style.display = 'none'
-     let content = document.querySelector('.form-container')
-     content.style.display='flex'
-})
