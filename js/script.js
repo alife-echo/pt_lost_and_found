d4eca98ae2546fdd441d06adefb7b86d7ef93839
alife-echo <alifesilva90@gmail.com>
2023-07-04 01:01:22 -0300

    fp

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
-let show = true
-document.querySelector('.menu-toggle').addEventListener('click',()=>{
-    
-    let one = document.querySelector('.menu-toggle .one')
-    let two = document.querySelector('.menu-toggle .two')
-    let three = document.querySelector('.menu-toggle .three')
-    let links_hidden = document.querySelector('.links-hidden')
+let show = true;
+const menuToggle = document.querySelector('.menu-toggle');
+const one = document.querySelector('.menu-toggle .one');
+const two = document.querySelector('.menu-toggle .two');
+const three = document.querySelector('.menu-toggle .three');
+const linksHidden = document.querySelector('.links-hidden');
 
-    if(show === true){
+menuToggle.addEventListener('click', () => {
+    if (show) {
         one.style.transform = 'rotate(45deg) translate(7px, 7px)';
-        two.style.opacity = 0
-        three.style.transform = 'rotate(-45deg) translate(8px,-10px)'
-        links_hidden.style.display = 'block'
-        show = false
-    }
-    else{
+        two.style.opacity = 0;
+        three.style.transform = 'rotate(-45deg) translate(8px, -10px)';
+        linksHidden.style.display = 'block';
+        show = false;
+    } else {
         one.style.transform = 'none';
-        two.style.opacity = 1
-        three.style.transform = 'none'
-        links_hidden.style.display = 'none'
-        show = true
+        two.style.opacity = 1;
+        three.style.transform = 'none';
+        linksHidden.style.display = 'none';
+        show = true;
     }
-   
-})
+});
